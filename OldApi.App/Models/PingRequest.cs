7dc4c6166123d4a8f15bcee7481a240fbe7bc3d4
Kris <kris_s2@outlook.com>
2025-10-24 22:49:44 -0700

    kata: adding implementation for request and response.

--- OldApi.App/Models/PingRequest.cs
+++ OldApi.App/Models/PingRequest.cs
@@ -7,10 +7,15 @@ namespace OldApi.App.Models
 {
 	public class PingRequest
 	{
+		public int Id { get; set; }
+		public string Message { get; set; }
 	}
 
 	public class PingResponse 
 	{
-
+		public int Id { get; set; }
+		public string Message { get; set; }
+		public string Status { get; set; }
+		public System.DateTime Timestamp { get; set; }
 	}
 }
