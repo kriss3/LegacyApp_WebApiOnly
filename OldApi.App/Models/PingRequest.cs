f2099ace294550294ffecab119a73f50572573fb
Kris <kris_s2@outlook.com>
2025-10-24 22:53:39 -0700

    kata: finish working on model

--- OldApi.App/Models/PingRequest.cs
+++ OldApi.App/Models/PingRequest.cs
@@ -1,21 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace OldApi.App.Models
+﻿namespace OldApi.App.Models
 {
 	public class PingRequest
 	{
 		public int Id { get; set; }
 		public string Message { get; set; }
-	}
-
-	public class PingResponse 
-	{
-		public int Id { get; set; }
-		public string Message { get; set; }
-		public string Status { get; set; }
-		public System.DateTime Timestamp { get; set; }
 	}
 }
---
+++ OldApi.App/Models/PingResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OldApi.App.Models
+{
+	public class PingResponse 
+	{
+		public int Id { get; set; }
+		public string Message { get; set; }
+		public string Status { get; set; }
+		public DateTime Timestamp { get; set; }
+	}
+}
