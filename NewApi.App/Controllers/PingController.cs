141c0588d1eefcba0e3e9d439e92379497602719
Kris <kris_s2@outlook.com>
2025-10-27 23:29:44 -0700

    kata: adding Ping controller to new API. Removing minimal api.

---
+++ NewApi.App/Controllers/PingController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewApi.App.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PingController : ControllerBase
+{
+}
